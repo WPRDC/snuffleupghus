68526c6fc1c3dc0f1f20f0954b23d50a76d3b26b
drw <drw@users.noreply.github.com>
2018-01-10 23:03:47 -0500

    Refactor to handle the basic extraction and transformation for the first table

--- snuffleupghus.py
+++ snuffleupghus.py
@@ -1,6 +1,6 @@
-import sys, csv
+import sys, csv, requests
 
-
+from pprint import pprint
 
 def write_to_csv(filename,list_of_dicts,keys):
     with open(filename, 'wb') as output_file:
@@ -8,27 +8,86 @@ def write_to_csv(filename,list_of_dicts,keys):
         dict_writer.writeheader()
         dict_writer.writerows(list_of_dicts)
 
-filepath = sys.argv[1]
-f = open(filepath,'r')
-f.next() # Skip the first line, since it gives the delimiter.
-reader = csv.DictReader(f, delimiter='|', quotechar='"') 
-list_of_dicts = list(reader) 
-f.close()
-f = open(filepath,'r')
-_ = f.readline()
-headers = f.readline().split('|')
-write_to_csv('output.csv',list_of_dicts,headers)
 
-# Split geocoordinates field into new latitude and longitude fields
+def parse_file(filepath):
+    # Use local file
+    f = open(filepath,'r')
+    f.next() # Skip the first line, since it gives the delimiter.
+    reader = csv.DictReader(f, delimiter='|', quotechar='"') 
+    list_of_dicts = list(reader) 
+    f.close()
+    f = open(filepath,'r')
+    _ = f.readline()
+    headers = f.readline().split('|')
+    #write_to_csv('output.csv',list_of_dicts,headers)
+    return list_of_dicts, headers
+
+def get_lat_and_lon(e,d):
+    # Split geocoordinates field ("Program Lat and Long") into new latitude and longitude fields
+    # 
+    latpart, lonpart = e['Program Lat and Long'].split(',')
+    _, latitude = latpart.split(': ')
+    _, longitude = lonpart.split(': ')
+    d['longitude'] = longitude
+    d['latitude'] = latitude
+    return d
+
+def fuse_cats(e,d):
+    # Combine Category One and Category Two into a |-delimited category field
+    cat1 = e['Category One']
+    cat2 = e['Category Two'] # Empty fields are empty strings (since we're loading a CSV file).
+    if len(cat2) == 0:
+        if len(cat1) == 0:
+            d['category'] = None
+        else:
+            d['category'] = cat1
+    elif len(cat1) == 0:
+        d['category'] = cat2
+    else:
+        d['category'] = "{}|{}".format(cat1,cat2)
+    return d
+
+def main():
 
 
+    # Down here in the main function, fetch all three CSV files with requests.
+    #   events.csv, safePlaces.csv, services.csv
+    # Then process them according to their needs.
 
-# Down here in the main function, fetch all three CSV files with requests.
+    if len(sys.argv) in [0,1,2,3,4]:
+        # Interpret command-line arguments as local filenames to use.
+        events_shelf, events_headers = parse_file(sys.argv[1]) # Where a shelf is a list of dictionaries
 
-# Then process them according to their needs.
+    else:
+        r = requests.get("http://bigburgh.com/csvdownload/events.csv")
+        events_shelf = r.json()
+#"http://bigburgh.com/csvdownload/safePlaces.csv"
+#"http://bigburgh.com/csvdownload/services.csv"
 
-# Reformat their field names.
+    events = [] 
+    # Reformat their field names.
+    for e in events_shelf:
+        d = {'event_name': e['Event Name'],
+            'recurrence': e["Recurring, One-Time or One-on-One?"],
+            'program_or_facility': e['Program (Facility) Name'],
+            'neighborhood': e['Program Neighborhood'],
+            'address': e['Program Address'],
+            'organization': e['Organization Name'],
+            'recommended_for': e['(Event) Recommended For :'],
+            'event_phone': e['Event Phone'],
+            'event_narrative': e['Event Narrative'],
+            'schedule': e['Schedule'],
+            'holiday_exception': e['Holiday Exception']
+            }
+        d = get_lat_and_lon(e,d)
+        d = fuse_cats(e,d)
 
-#for events.csv
-#Event Name|Recurring, One-Time or One-on-One?|Program (Facility) Name|Program Neighborhood|Program Address|Program Lat and Long|Organization Name|Category One|Category Two|(Event) Recommended For :|(Event) Requirements|Event Phone|Event Narrative|Schedule|Holiday Exception ==> 
-#event_name, recurrence, program_or_facility, neighborhood, address, latitude, longitude, organization, category1
+        events.append(d)
+    events_fields = ['event_name','recurrence','program_or_facility','neighborhood','address','latitude','longitude','organization','category','recommended_for','event_phone','event_narrative','schedule','holiday_exception']
+    # Then bring in the schema and ETL framework.
+
+
+if __name__ == '__main__':
+    print(len(sys.argv))
+    if len(sys.argv) == 2:
+        main() # Make this the default.
